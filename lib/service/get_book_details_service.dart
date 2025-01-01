04a507c2ce9bec0578f99aec289d99515baa22df
Hiba-Kaboul <hibakaboul@gmail.com>
2025-01-01 19:03:04 +0300

    login_signup_homepage_profile

--- lib/service/get_book_details_service.dart
+++ lib/service/get_book_details_service.dart
@@ -1,33 +1,26 @@
-
-
 import 'package:dio/dio.dart';
 import 'package:languages_project/config/service_locator.dart';
 import 'package:languages_project/model/books_detail_model.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
-Future<List<BooksDetailModel>> getBooksDetailes(int idbook) async {
-
+Future<BooksDetailModel> getBooksDetailes(int idbook) async {
   try {
-  Dio dio = Dio();
-  Response response = await dio.get(
-    "http://127.0.0.1:8000/api/bookDetail/${idbook}",
-    options: Options(
-      headers: {
-        "Authorization":
-            "Bearer ${servicelocator.get<SharedPreferences>().getString('token')}",
-      },
-    ),
-  );
-  List<BooksDetailModel> details = [];
-  for (var i = 0; i < response.data.length; i++) {
-    BooksDetailModel booksdetails = BooksDetailModel.fromMap(response.data[i]);
-    details.add(booksdetails);
-  }
+    Dio dio = Dio();
+    Response response = await dio.get(
+      "http://127.0.0.1:8000/api/bookDetail/${idbook}",
+      options: Options(
+        headers: {
+          "Authorization":
+              "Bearer ${servicelocator.get<SharedPreferences>().getString('token')}",
+        },
+      ),
+    );
 
-  return details;
-    
+    BooksDetailModel booksdetails = BooksDetailModel.fromMap(response.data);
+
+    return booksdetails;
   } catch (e) {
     print(e);
-    return [];
+    throw e;
   }
 }
--- lib/view/book_details_page.dart
+++ lib/view/book_details_page.dart
@@ -16,21 +16,136 @@ class BookDetailsPage extends StatelessWidget {
           future: getBooksDetailes(id),
           builder: (context, snapshot) {
             if (snapshot.hasData) {
-              return Card(
-                
+              return SizedBox(
+                width: 500,
+                height: 700,
+                child: Card(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  surfaceTintColor: Colors.cyan,
+                  elevation: 20,
+                  shadowColor: const Color.fromARGB(255, 168, 124, 176),
+                  borderOnForeground: true,
+                  child: Column(
+                    children: [
+                      Container(
+                        height: 400,
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(20),
+                            image: DecorationImage(
+                                image: NetworkImage("http://127.0.0.1:8000" +
+                                    snapshot.data!.image+"${id}"),
+                                fit: BoxFit.cover)),
+                      ),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      Padding(
+                        padding: EdgeInsets.only(right: 350),
+                        child: Text(
+                          snapshot.data!.name+"${id}",
+                          style: TextStyle(
+                              //fontSize: 35,
+                              ),
+                        ),
+                      ),
+                      Text(
+                        snapshot.data!.author+"${id}",
+                        style: TextStyle(
+                          // fontSize: 20,
+                          color: Colors.grey,
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.only(right: 260.0),
+                        child: Text(
+                          snapshot.data!.price+"${id}",
+                          style: TextStyle(
+                            // fontSize: 20,
+                            color: Colors.grey,
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.only(right: 260.0),
+                        child: Text(
+                          snapshot.data!.ratings+"${id}",
+                          style: TextStyle(
+                            //fontSize: 20,
+                            color: Colors.grey,
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.only(right: 260.0),
+                        child: Text(
+                          snapshot.data!.details+"${id}",
+                          style: TextStyle(
+                            // fontSize: 20,
+                            color: Colors.grey,
+                          ),
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 30,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.only(right: 20),
+                            child: Container(
+                              width: 100,
+                              height: 40,
+                              decoration: BoxDecoration(
+                                  color: Colors.cyan,
+                                  borderRadius: BorderRadius.circular(20)),
+                              child: const Center(
+                                child: Text("add to cart",
+                                    style: TextStyle(
+                                      color: Colors.white,
+                                      fontSize: 20,
+                                    )),
+                              ),
+                            ),
+                          ),
+                          Container(
+                            margin: EdgeInsets.only(right: 20),
+                            width: 100,
+                            height: 40,
+                            decoration: BoxDecoration(
+                              border: Border.all(color: Colors.cyan),
+                              color: Colors.white,
+                              borderRadius: BorderRadius.circular(20),
+                            ),
+                            child: const Center(
+                              child: Text("add to favorait",
+                                  style: TextStyle(
+                                    color: Colors.cyan,
+                                    fontSize: 20,
+                                  )),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
               );
             } else {
-              return CircularProgressIndicator();
+              return Center(child: CircularProgressIndicator());
             }
           },
         )
 
+
+        );
+  }
+}
         //  Center(
         //     child: Text(
         //   "Books ${id}",
         //   style: TextStyle(fontSize: 30),
         // )
         // ),
-        );
-  }
-}
